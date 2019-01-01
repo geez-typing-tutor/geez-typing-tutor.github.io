da827ece4458dff11df179455c3c3b8134df6f43
drenzbot <>
2019-01-01 11:02:12 +0300

    Initial functional version

---
+++ precache-manifest.5efa1b8247cf25ab866b00daeecb3512.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9d3a863b9754c24bc2967d8e98c22b2",
+    "url": "/static/media/hand-placement.b9d3a863.png"
+  },
+  {
+    "revision": "7a5dee49ace3248768281a192bb4d166",
+    "url": "/static/media/fidel-background.svg.7a5dee49.png"
+  },
+  {
+    "revision": "c8ff996668ae67d316ff4d82e6f7102f",
+    "url": "/static/media/NotoSansEthiopic-RegularMonospaced.c8ff9966.ttf"
+  },
+  {
+    "revision": "2a92090f2b919130a31d2dfd582619eb",
+    "url": "/static/media/NotoSansEthiopic-Regular.2a92090f.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d72f3b7e0d0dcf64d991",
+    "url": "/static/js/main.d72f3b7e.chunk.js"
+  },
+  {
+    "revision": "8e8f5def146e5180bb09",
+    "url": "/static/js/1.8e8f5def.chunk.js"
+  },
+  {
+    "revision": "d72f3b7e0d0dcf64d991",
+    "url": "/static/css/main.409f2a68.chunk.css"
+  },
+  {
+    "revision": "b2271744772621bcff767fe29d4c8473",
+    "url": "/index.html"
+  }
+];
